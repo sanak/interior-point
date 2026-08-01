14316447615f42faa131bd5b67d351c36548fc41
Ko Nagase <geosanak@gmail.com>
2026-08-01 18:55:35 +0900

    feat(js): export the verify surface from the package

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,2 +1,3 @@
 export { interiorPoint } from "./algorithm/InteriorPoint.ts";
+export { verifyInteriorPoint, isVerified, InteriorPointVerification } from "./VerifyInteriorPoint.ts";
 export type { Coordinate } from "./GeometryAdapter.ts";
