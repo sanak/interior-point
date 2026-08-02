724f3695dca1f0dfeeef8725ec8f2c6a6b182c28
Ko Nagase <geosanak@gmail.com>
2026-08-02 22:57:12 +0900

    feat(js): export centroidFirstInteriorPoint from the package

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,3 +1,4 @@
 export { interiorPoint } from "./algorithm/InteriorPoint.ts";
+export { centroidFirstInteriorPoint } from "./CentroidFirstInteriorPoint.ts";
 export { verifyInteriorPoint, isVerified, InteriorPointVerification } from "./VerifyInteriorPoint.ts";
 export type { Coordinate } from "./GeometryAdapter.ts";
